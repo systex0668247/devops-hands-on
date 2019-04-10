ed425b148822c8c8f007b943929cbaf1bbcde64e
Abola Lee <abola921@gmail.com>
2019-04-10 18:48:44 +0800

    Delete logging.py

--- sample/hello/logging.py
+++
@@ -1,8 +0,0 @@
-import os,logging
-
-# logging setting
-os.makedirs("logs", exist_ok=True)
-logging.basicConfig(filename='logs/myapp.log',level=logging.DEBUG)
-
-if __name__ == '__main__':
-    logging.info( os.environ.get('HOSTNAME') or 'unknown' )
