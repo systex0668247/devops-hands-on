0aaadbc3b9839fe67491405a59c5e0f88fac4286
HebeKuo <hebekuo@systex.com>
2019-04-22 11:34:47 +0800

    Update check-project.sh

--- check-project.sh
+++ check-project.sh
@@ -14,7 +14,8 @@ checkProject() {
     GOOGLE_PROJECT_ID=systex-lab-$(cat /proc/sys/kernel/random/uuid | cut -b -6)
     echo "  未定義 GOOGLE_PROJECT_ID.   由系統自動產生...(GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID)" 
     
-    gcloud projects create $GOOGLE_PROJECT_ID
+    gcloud projects create $GOOGLE_PROJECT_ID    
+    echo "GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID" | tee -a ~/.profile
     
     BILLING_ACCOUNT=$(gcloud beta billing accounts list | grep True | awk -F" " '{print $1}')
     gcloud beta billing projects link $GOOGLE_PROJECT_ID --billing-account $BILLING_ACCOUNT
@@ -23,7 +24,6 @@ checkProject() {
   fi
 
   gcloud config set project $GOOGLE_PROJECT_ID
-  echo "GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID" | tee -a ~/.profile
 }
 
 checkProject
