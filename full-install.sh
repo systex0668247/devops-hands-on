61cad504cd2c19f0c5b53ab7619086b326bdd22c
Abola Lee <abola921@gmail.com>
2019-06-20 11:30:36 +0800

    fixed default docker config.json name error

--- full-install.sh
+++ full-install.sh
@@ -177,7 +177,7 @@ installJenkins() {
   gsutil iam ch serviceAccount:jenkins-deployer@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com:admin gs://artifacts.${GOOGLE_PROJECT_ID}.appspot.com/  > /dev/null 2>&1
   gcloud iam service-accounts keys create key.json --iam-account=jenkins-deployer@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com > /dev/null 2>&1
   docker login -u _json_key -p "$(cat key.json)" https://gcr.io  > /dev/null 2>&1
-  kubectl create configmap google-container-key --from-file=.docker/key.json  > /dev/null 2>&1
+  kubectl create configmap google-container-key --from-file=.docker/config.json  > /dev/null 2>&1
 
   kubectl create sa jenkins-deployer > /dev/null 2>&1
   kubectl create clusterrolebinding jenkins-deployer-role --clusterrole=cluster-admin --serviceaccount=default:jenkins-deployer > /dev/null 2>&1
