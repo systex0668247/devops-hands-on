01f463385acd56549a69936c95afc35c10f945a0
harryliu123 <harryliu123@users.noreply.github.com>
2019-07-09 11:41:07 +0800

    Update full-install-aws.sh

--- full-install-aws.sh
+++ full-install-aws.sh
@@ -3,95 +3,64 @@
 # 安裝 Lab(k8s) 課程環境
 ###
 
-# 確認使用的是 root user
-checkRootUser() {
-  if [ `whoami | grep ^root$ | wc -l` -eq 0 ];then
-    echo "[ERROR] 請使用 root 執行"
-    echo "        請先執行以下指令，切換為 root 後再重試 "
-    echo "------------------------------------------"
-    echo "sudo su"
-    echo "------------------------------------------"
-    exit
-  fi
+#!/usr/bin/env bash
+###########
+# 專案 AWS 建立
+###
+
+
+# 安裝aws cli
+sudo apt-get -y install python3.6 python3-pip
+pip3 install awscli --upgrade --user
+echo "到AWS 的IAM 上取得帳號的 Access Key ID 和 Secret access key"
+
+# 確認使用者是否登入
+echo "輸入剛剛取得的 Access Key ID 和 Secret access key"
+aws configure
+
+
+# install eks
+installeks() {
+cd $CURRENT_HOME
+# 下載安裝包
+git clone https://github.com/harryliu123/eks-templates
+cd eks-templates
+
+# 建立VPC
+aws cloudformation create-stack  --stack-name ${VPC_STACK_NAME} --template-body file://eks-vpc.yaml --region us-east-2
+sleep 5
+vpcid=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=${VPC_STACK_NAME}-VPC |jq -r  '.Vpcs[].VpcId')
+Subnet01=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=${VPC_STACK_NAME}-Subnet01 |jq -r '.Subnets[].SubnetId')
+Subnet02=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=${VPC_STACK_NAME}-Subnet02 |jq -r '.Subnets[].SubnetId')
+Subnet03=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=${VPC_STACK_NAME}-Subnet03 |jq -r '.Subnets[].SubnetId')
+
+# 建立AmazonEKSAdminRole IAM Role
+iamrole=$(aws iam get-role --role-name AmazonEKSAdminRole --query 'Role.Arn' --output text)
+
+# 部屬 EKS
+EKS_ADMIN_ROLE=$iamrole VPC_ID=$vpcid SUBNET1=$Subnet01 SUBNET2=$Subnet02 SUBNET3=$Subnet03  make create-eks-cluster
+sleep 36000
 }
 
-# 確認 gcloud 指令已經 login
-checkGcloudLogin() {
-  if [ `gcloud config get-value core/account | grep 'compute@developer.gserviceaccount.com' | wc -l` -eq 1 ]; then
-    echo "[ERROR] gcloud 指令尚未登入"
-    echo "        請先執行以下指令，登入 gcloud 後再重試 "
-    echo "------------------------------------------"
-    echo "gcloud auth login"
-    echo "------------------------------------------"
-    exit
-  fi
+# 確認CloudFormation 狀態是否完成
+checkeksstatus() {
+while true; do 
+	status=$(aws cloudformation describe-stacks --stack-name eksdemo |jq -r '.Stacks[].StackStatus')
+	while ($status -ne "CREATE_COMPLETE"); do 
+	    echo "EKS 還在建立中"
+		sleep 3600
+	done
+	echo $status
+done
 }
 
-# 設定參數
-initParameter() {
-  echo "參數設定確認中..."
-  
-  GOOGLE_PROJECT_ID=$(gcloud projects list --filter=systex-lab  | awk 'END {print $1}')
-  
-  # GOOGLE_PROJECT_ID
-  if [ -z $GOOGLE_PROJECT_ID  ]; then
-    GOOGLE_PROJECT_ID=systex-lab-$(cat /proc/sys/kernel/random/uuid | cut -b -6)
-    echo "  未定義 GOOGLE_PROJECT_ID.   由系統自動產生...(GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID)" 
-  else
-    echo "  系統參數 GOOGLE_PROJECT_ID  已設定...........(GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID)" 
-  fi
-  
-  # GOOGLE_ZONE
-  if [ -z $GOOGLE_ZONE  ]; then
-    GOOGLE_ZONE=asia-east1-$(ary=(a b c) && echo ${ary[$(($RANDOM%3))]})
-    echo "  未定義 GOOGLE_ZONE.         使用預設值.......(GOOGLE_ZONE=$GOOGLE_ZONE)"
-  else
-    echo "  系統參數 GOOGLE_ZONE        已設定...........(GOOGLE_ZONE=$GOOGLE_ZONE)" 
-  fi
-  
-  # GOOGLE_GKE_NAME
-  if [ -z $GOOGLE_GKE_NAME  ]; then
-    GOOGLE_GKE_NAME=devops-camp
-    echo "  未定義 GOOGLE_GKE_NAME.     使用預設值.......(GOOGLE_GKE_NAME=$GOOGLE_GKE_NAME)"
-  else
-    echo "  系統參數 GOOGLE_GKE_NAME    已設定...........(GOOGLE_GKE_NAME=$GOOGLE_GKE_NAME)" 
-  fi
-
-  # GOOGLE_GKE_MACHINE
-  if [ -z $GOOGLE_GKE_MACHINE  ]; then
-    GOOGLE_GKE_MACHINE=n1-standard-2
-    echo "  未定義 GOOGLE_GKE_MACHINE.  使用預設值.......(GOOGLE_GKE_MACHINE=$GOOGLE_GKE_MACHINE)"
-  fi
-
-  # GOOGLE_GKE_NODES
-  if [ -z $GOOGLE_GKE_NODES  ]; then
-    GOOGLE_GKE_NODES=3
-    echo "  未定義 GOOGLE_GKE_NODES.    使用預設值.......(GOOGLE_GKE_NODES=$GOOGLE_GKE_NODES)"
-  fi
-
-  # GOOGLE_GCE_IMAGE
-  #if [ -z $GOOGLE_GKE_VERSION  ]; then
-  #  GOOGLE_GKE_VERSION=1.12.8-gke.6
-  #  echo "  未定義 GOOGLE_GKE_VERSION.  使用預設值.......(GOOGLE_GKE_VERSION=$GOOGLE_GKE_VERSION)"
-  #fi
-
-  # HELM_VERSION
-  if [ -z $HELM_VERSION  ]; then
-    HELM_VERSION=v2.13.1
-    echo "  未定義 HELM_VERSION.        使用預設值.......(HELM_VERSION=$HELM_VERSION)"
-  fi
-
-  # ISTIO_VERSION
-  if [ -z $ISTIO_VERSION  ]; then
-    ISTIO_VERSION=1.0.8
-    echo "  未定義 ISTIO_VERSION.       使用預設值.......(ISTIO_VERSION=$ISTIO_VERSION)"
-  fi
-
-  read -p "確認開始安裝(Y/n)?" yn
-  case $yn in
-      [Nn]* ) echo "動作取消 "; exit;;
-  esac  
+# 新建ECR
+createecr() {
+aws ecr create-repository --repository-name ecr
+$(aws ecr get-login --no-include-email --region us-west-2)
+echo "ecr的 token 在 $CURRENT_HOME/.docker/config.json"
 }
+
 
 # 安裝 kubectl 指令
 installKubectl() {
@@ -100,46 +69,17 @@ installKubectl() {
   yum -y install kubectl > /dev/null 2>&1 && echo "完成"
 }
 
-createProject() {
-  echo "正在建立GCP 專案..."
-  gcloud projects create $GOOGLE_PROJECT_ID > /dev/null 2>&1
-  echo "export \$(cat .my-env|xargs)" | tee -a ~/.profile > /dev/null 2>&1
-
-  printf "  切換專案至($GOOGLE_PROJECT_ID)..."
-  gcloud config set project $GOOGLE_PROJECT_ID > /dev/null 2>&1 && echo "完成"
-    
-    
-  BILLING_ACCOUNT=$(gcloud beta billing accounts list | grep True | awk -F" " '{print $1}')
-  gcloud beta billing projects link $GOOGLE_PROJECT_ID --billing-account $BILLING_ACCOUNT > /dev/null 2>&1
+# 執行更新IAM role
+updaterole(){
+aws iam update-assume-role-policy --role-name AmazonEKSAdminRole --policy-document file://assume-role-policy.json
 }
 
-createK8S() {
-  echo "正在建立GKE..."
-  
-  printf "  啟用 Container API..."
-  gcloud services enable container.googleapis.com --project=$GOOGLE_PROJECT_ID  > /dev/null 2>&1 && echo "完成"
-
-  printf "  開始建立 GKE($GOOGLE_GKE_NAME)..."
-  if [ $(gcloud container clusters list --project=$GOOGLE_PROJECT_ID | grep $GOOGLE_GKE_NAME | wc -l) -eq 0 ]; then
-    gcloud container clusters create $GOOGLE_GKE_NAME \
-        --project=$GOOGLE_PROJECT_ID \
-        --machine-type=$GOOGLE_GKE_MACHINE \
-        --zone=$GOOGLE_ZONE \
-        --num-nodes=$GOOGLE_GKE_NODES \
-        --disk-type "pd-standard" \
-        --disk-size "500" \
-        > /dev/null 2>&1 && echo "完成"
-  else
-    echo "已存在"
-  fi
-  
-  printf "  正在設定授權..."
-  kubectl create clusterrolebinding cluster-admin-binding \
-    --clusterrole=cluster-admin \
-    --user=$(gcloud config get-value core/account)\
-    > /dev/null 2>&1 && echo "完成"
-
+# 更新kubectl configure
+updatekubectlconfigure() {
+aws --region us-east-2 eks update-kubeconfig --name eksdemo --role-arn $iamrole
 }
+
+
 
 installHelm() {
 
@@ -277,6 +217,8 @@ installEFK() {
   kubectl apply -f devops-hands-on/logging-efk.yaml > /dev/null 2>&1 && echo "完成"
 
 }
+
+
 
 installKSM() {
   cd $CURRENT_HOME
