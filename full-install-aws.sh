07eadc92f170d35356dabf7e4bbaeb8d56ac8cb4
harryliu123 <harryliu123@users.noreply.github.com>
2019-07-09 14:50:45 +0800

    Update full-install-aws.sh

--- full-install-aws.sh
+++ full-install-aws.sh
@@ -39,6 +39,10 @@ Subnet02=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=${VPC_STACK_N
 Subnet03=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=${VPC_STACK_NAME}-Subnet03 |jq -r '.Subnets[].SubnetId')
 
 # AmazonEKSAdminRole IAM Role
+aws iam create-role --role-name AmazonEKSAdminRole --assume-role-policy-document file://assume-role-policy.json
+aws iam attach-role-policy --role-name AmazonEKSAdminRole --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
+aws iam attach-role-policy --role-name AmazonEKSAdminRole --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws iam put-role-policy --role-name AmazonEKSAdminRole --policy-name EKSAdminExtraPolicies --policy-document file://eks-admin-iam-policy.json
 iamrole=$(aws iam get-role --role-name AmazonEKSAdminRole --query 'Role.Arn' --output text)
 
 # 部屬 EKS
