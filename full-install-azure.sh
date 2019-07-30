e0398a5db0355a00e0653898d1472ddae6639b99
harryliu123 <harryliu123@users.noreply.github.com>
2019-07-30 17:46:10 +0800

    Update full-install-azure.sh

--- full-install-azure.sh
+++ full-install-azure.sh
@@ -2,7 +2,7 @@
 # 無法在Azure shell或是 aws shell上執行, 因為沒有docker daemon可以使用
 # 請使用 在GCP 的cloud : https://console.cloud.google.com
 # 新增請直接執行      bash <(curl -L https://raw.githubusercontent.com/harryliu123/devops-hands-on/master/full-install-azure.sh)
-# 刪除所有資源請執行  bash <(curl -L https://raw.githubusercontent.com/harryliu123/devops-hands-on/master/full-install-azure.sh del)
+# 刪除所有資源請執行  bash <(curl -L https://raw.githubusercontent.com/harryliu123/devops-hands-on/master/full-install-azure.sh del) 
 #######################################################
 Random=$(cat /proc/sys/kernel/random/uuid | cut -b -6)
 
