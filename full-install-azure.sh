0778e7637986d2b886094b83755d5f8681dcf227
harryliu123 <harryliu123@users.noreply.github.com>
2019-07-30 09:44:23 +0800

    Update full-install-azure.sh

--- full-install-azure.sh
+++ full-install-azure.sh
@@ -12,23 +12,6 @@ PASSWORD_WIN="P@ssw0rd1234"
 myAKSClustername=LabAKSCluster$Random
 ######################################################
 
-# 執行
-installazcli
-azlogin
-ResourceGroupCreate
-AKSCreate
-kubeconfig
-setwnodeNoSchedule
-changestorageclass
-installistio
-InstallAcrJenkins
-installEFK
-installKSM
-outputingress
-printVirtualService
-windowspodInwnode
-EnvAndMessage
-
 #######################################################
 
 installazcli(){
@@ -420,3 +403,22 @@ Jenkins CI/CD          : http://jenkins.$INGRESS_HOST.nip.io/
 -------------------------------------------------------------
 EOF
 }
+
+
+###############################################################
+# 執行
+installazcli
+azlogin
+ResourceGroupCreate
+AKSCreate
+kubeconfig
+setwnodeNoSchedule
+changestorageclass
+installistio
+InstallAcrJenkins
+installEFK
+installKSM
+outputingress
+printVirtualService
+windowspodInwnode
+EnvAndMessage
