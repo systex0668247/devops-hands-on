6e4c500f899c022ee64f70152c084f5f194090d4
harryliu123 <harryliu123@users.noreply.github.com>
2019-08-05 13:14:59 +0800

    Update full-install-aws.sh

--- full-install-aws.sh
+++ full-install-aws.sh
@@ -12,21 +12,11 @@ AWS_ACCOUT_ID=<輸入自己的accout_id>             # root User ID，例如 348
 iamuseraccount=<請變更自己的AWS上的IAM user>     # IAM 使用者名稱，例如 A506-Harry
 # 執行 Script 時會需要在 互動介面輸入 AWS 程式存取金鑰，請事先產生及複製保存 Access Key ID 和 Secret access key
 CURRENT_HOME=$(pwd)                           # 設定家目錄為預設工作目錄的參數
-############################################################################
-# 清除前次執行所遺留的檔案   
-############################################################################
-rm -rf ~/.my-env
-rm -rf key.json
-rm -rf devops-hands-on
-
-############################################################################
-# 下載 Script 所需資源   
-############################################################################
-git clone https://github.com/abola/devops-hands-on.git
 
 ##########################
 ### 逐步執行的function
 ##########################
+main() {
 initialclientcloudshell
 installeks
 checkeksstatus
@@ -35,15 +25,17 @@ installKubectl
 updaterole
 updatekubectlconfigure
 createiamgroup
-createhaproxy
+installkeycloak
+InstallEcrJenkins
+installistio
+installEFK
+installKSM
+setupService
+#createhaproxy
 
 
 > ~/.my-env
-echo "GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID" >> ~/.my-env
-echo "GOOGLE_ZONE=$GOOGLE_ZONE" >> ~/.my-env
-echo "GOOGLE_GKE_NAME=$GOOGLE_GKE_NAME" >> ~/.my-env
 echo "INGRESS_HOST=$INGRESS_HOST" >> ~/.my-env
-echo "KeycloakPW=$keycloakpw" >> ~/.my-env
 cat <<EOF
 -------------------------------------------------------------
 環境安裝完成
@@ -54,13 +46,10 @@ Kiali Service Graph    : http://kiali.$INGRESS_HOST.nip.io/
 Jaeger Tracing         : http://jaeger.$INGRESS_HOST.nip.io/
 Kibana Logging         : http://kibana.$INGRESS_HOST.nip.io/
 Jenkins CI/CD          : http://jenkins.$INGRESS_HOST.nip.io/
-Keycloak               : http://keycloak.$INGRESS_HOST.nip.io/    $keycloakpw
+Keycloak               : http://keycloak.$INGRESS_HOST.nip.io/
 -------------------------------------------------------------
 EOF
-
-
-
-
+}
 
 
 initialclientcloudshell(){
@@ -151,13 +140,251 @@ aws --region $AWS_REGION eks update-kubeconfig --name eksdemo --role-arn $iamrol
 }
 
 
-
 installkeycloak(){
 helm install --name keycloak -f keycloak-values.yaml stable/keycloak
 # keycloakpw=$(kubectl get secret --namespace default keycloak-http -o jsonpath="{.data.password}" | base64 --decode)
 echo "安裝 keycloak，帳號為 admin  密碼為 systex "
 }
 
+
+
+installistio(){
+# 建立istio 
+echo "安裝istio 1.1.2"
+ISTIO_VERSION=1.1.2 > /dev/null 2>&1
+curl -sL "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istio-$ISTIO_VERSION-linux.tar.gz" | tar xz > /dev/null 2>&1
+
+cd istio-$ISTIO_VERSION > /dev/null 2>&1
+cp ./bin/istioctl /usr/local/bin/istioctl > /dev/null 2>&1
+chmod +x /usr/local/bin/istioctl > /dev/null 2>&1
+export PATH=$PATH:$HOME/istio-$ISTIO_VERSION/bin/ > /dev/null 2>&1
+
+
+# tiller
+kubectl apply -f install/kubernetes/helm/helm-service-account.yaml > /dev/null 2>&1
+helm init --service-account tiller --node-selectors "beta.kubernetes.io/os"="linux"  > /dev/null 2>&1
+sleep 10
+
+## 安裝istio 加入其他工具
+kubectl create namespace istio-system > /dev/null 2>&1 
+sleep 1
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kiali
+  namespace: istio-system
+  labels:
+    app: kiali
+type: Opaque
+data:
+  username: YWRtaW4=
+  passphrase: c3lzdGV4
+EOF
+
+  helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f - > /dev/null 2>&1
+  helm template install/kubernetes/helm/istio \
+    --name istio --namespace istio-system \
+    --set sidecarInjectorWebhook.enabled=true \
+    --set pilot.traceSampling=1.0 \
+    --set pilot.resources.requests.cpu=100m \
+    --set pilot.resources.requests.memory=256Mi \
+    --set grafana.enabled=true \
+    --set tracing.enabled=true \
+    --set servicegraph.enabled=true \
+    --set kiali.enabled=true \
+    --set kiali.createDemoSecret=true \
+	--set gateways.istio-egressgateway.enabled=false \
+	--set gateways.istio-ingressgateway.sds.enabled=true \
+  |  kubectl apply -f - > /dev/null 2>&1
+  printf "等待服務啟動中..."
+  while [ `kubectl get po -n istio-system | grep istio-sidecar-injector | grep Running | grep '1/1' | wc -l` -eq 0 ]
+  do
+    sleep 1
+  done  
+  echo "完成"
+
+  printf "  設定自動注入 sidecar ..."
+  kubectl label namespace default istio-injection=enabled > /dev/null 2>&1 && echo "完成"
+
+  printf "  安裝 Bookinfo 範例程式 ..."
+  kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml > /dev/null 2>&1
+  kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml > /dev/null 2>&1
+  kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml > /dev/null 2>&1 && echo "完成"
+}
+
+
+InstallEcrJenkins(){
+
+# ECR & jenkins
+ # 建立IAM User 
+  aws iam create-user --user-name jenkine-ecr
+  
+  new_key=$(aws iam create-access-key --user-name jenkine-ecr  | jq -e -r .AccessKey)
+  jenkine_ACCESS_KEY_ID=$(printf "%s" $new_key | jq -e -r .AccessKeyId)
+  jenkine_SECRET_ACCESS_KEY=$(printf "%s" $new_key | jq -e -r .SecretAccessKey)
+  jenkine_arn=$(aws iam get-user --user-name jenkine-ecr | jq  -r '.User[]'|grep arn)
+  
+cat <<'EOF' > jenkins-ecr-role-assume.json 
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowPushPull",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": [
+          "push-pull-user-1"
+        ]
+      },
+      "Action": [
+        "sts:AssumeRole"
+      ]
+    }
+  ]
+}
+EOF
+
+
+cat <<'EOF' > jenkins-ecr-role.json 
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowPushPull",
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload"
+      ],
+	  "Resource":"*"
+	}
+  ]
+}
+EOF
+
+
+sed -i "s@push-pull-user-1@${jenkine_arn}@g" jenkins-ecr-role-assume.json
+#建立角色
+aws iam create-role --role-name jenkins-ecr-assumerole --assume-role-policy-document file://jenkins-ecr-role-assume.json
+#為角色建立內嵌許可政策
+aws iam put-role-policy --role-name jenkins-ecr-assumerole --policy-name jenkins-ecr-role --policy-document file://jenkins-ecr-role.json
+
+
+# https://github.com/awslabs/amazon-ecr-credential-helper
+# 建立 configmap for awscredentials_jenkins 放在jenkins的 ~/.aws/credentials
+echo "[default]" > awscredentials_jenkins
+echo "aws_access_key_id = $jenkine_ACCESS_KEY_ID" >> awscredentials_jenkins
+echo "aws_secret_access_key = $jenkine_SECRET_ACCESS_KEY" >> awscredentials_jenkins
+# kubectl create configmap aws-iam-jenkine-ecr-key --from-file=awscredentials_jenkins  > /dev/null 2>&1
+
+
+# 建立 configmap for awscredentials_jenkins 放在jenkins的 .docker/config.json
+#  348053640110.dkr.ecr.us-west-2.amazonaws.com
+cat <<'EOF' > docker-config.json
+{
+	"credHelpers": {
+		"aws_account_id.dkr.ecr.region.amazonaws.com": "ecr-login"
+	}
+}
+EOF
+
+sed -i "s/region/${AWS_REGION}/g" docker-config.json
+sed -i "s/aws_account_id/${AWS_ACCOUT_ID}/g" docker-config.json
+kubectl create configmap docker-registry-key --from-file=docker-config.json  > /dev/null 2>&1
+# kubectl create configmap google-container-key  --from-file=docker-config.json  > /dev/null 2>&1
+
+
+  echo "安裝 Jenkins ..."
+  git clone https://github.com/harryliu123/devops-hands-on.git > /dev/null 2>&1
+  kubectl create sa jenkins-deployer > /dev/null 2>&1
+  kubectl create clusterrolebinding jenkins-deployer-role --clusterrole=cluster-admin --serviceaccount=default:jenkins-deployer > /dev/null 2>&1
+  K8S_ADMIN_CREDENTIAL=$(kubectl describe secret jenkins-deployer | grep token: | awk -F" " '{print $2}')
+  cat <<EOF | kubectl apply -f -  > /dev/null 2>&1 && echo "完成"
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: fabric8-rbac
+subjects:
+  - kind: ServiceAccount
+    name: default
+    namespace: default
+roleRef:
+  kind: ClusterRole
+  name: cluster-admin
+  apiGroup: rbac.authorization.k8s.io
+EOF
+
+  printf "  正在安裝 jenkins-slave ... "
+  printf "create repository..." && aws ecr create-repository --repository-name=jnlp-slave > /dev/null 2>&1
+  printf "build..." && docker build --build-arg jenkine_ACCESS_KEY_ID=${jenkine_ACCESS_KEY_ID} jenkine_SECRET_ACCESS_KEY=${jenkine_SECRET_ACCESS_KEY} -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/jnlp-slave:v1 -f devops-hands-on/jenkins/slave/Dockerfileaws > /dev/null 2>&1
+  printf "push..." && docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/jnlp-slave:v1 > /dev/null 2>&1
+  echo "完成"
+
+  printf "  正在安裝 jenkins:lts ..."
+  helm install --name jenkins \
+    --set cloud.provider.aws=true \
+    --set Master.ServiceType=ClusterIP \
+    --set Master.K8sAdminCredential=$K8S_ADMIN_CREDENTIAL \
+    --set Agent.Image=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/jnlp-slave \
+    --set Agent.ImageTag=v1 \
+    --set Master.AdminPassword=systex \
+    --set Master.AwsAccountId=${AWS_ACCOUNT_ID} \
+    --set Master.AwsRegion=${AWS_REGION} \
+    --set configmap.docker.config_json=docker-registry-key \
+    devops-hands-on/jenkins > /dev/null 2>&1 && echo "完成"
+
+}
+
+installEFK() {
+  cd $CURRENT_HOME
+
+  echo "安裝 Elasticsearch + Fluentd + Kibana ..."
+  printf "  安裝中 ..."
+  kubectl apply -f devops-hands-on/logging-efk.yaml > /dev/null 2>&1 && echo "完成"
+
+}
+
+installKSM() {
+  cd $CURRENT_HOME
+
+  echo "安裝 Kube-state-metrics ..."
+  printf "  安裝中 ..."
+  kubectl apply -f devops-hands-on/kube-state-metrics/app-crd.yaml > /dev/null 2>&1 
+  kubectl apply -f devops-hands-on/kube-state-metrics/prometheus-metrics_sa_manifest.yaml --namespace logging > /dev/null 2>&1 
+  kubectl apply -f devops-hands-on/kube-state-metrics/prometheus-metrics_manifest.yaml --namespace logging  > /dev/null 2>&1 && echo "完成"
+
+}
+
+
+setupService() {
+  cd $CURRENT_HOME
+  
+  echo "設定對外服務項目..."
+  
+  printf "  等待對外IP配發中..."
+  while [ `kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' | wc -c` -eq 0 ]
+  do
+    sleep 1
+  done  
+  INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' | nslookup | grep Address | tail -n1 | awk -F" " '{print $2}') 
+  echo "(IP=$INGRESS_HOST)...完成"
+
+  printf "  開啟對外服務中..."
+  helm template --set istio.ingressgateway.ip=$INGRESS_HOST devops-hands-on/svc | kubectl apply -f - > /dev/null 2>&1 && echo "完成"
+}
+
+cd ~
+CURRENT_HOME=$(pwd)
+
+rm -rf ~/.my-env
+rm -rf key.json
 
 createhaproxy(){
 ingressgateway=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.EXTERNAL-IP[0].ip})
@@ -166,3 +393,5 @@ aws cloudformation create-stack  --stack-name  Haproxy-create --template-body fi
 sleep 20
 }
 
+
+main
